633127463fab7ce301bc1f49d429e678c7ae86c8
Lee Miller <dleemiller@protonmail.com>
2024-06-28 11:27:04 -0600

    fixing setup

---
+++ ragfile/metadata/__init__.py
@@ -0,0 +1,1 @@
+from .metadata_v1 import RagFileMetaV1
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 module = Extension(
-    "ragfile.ragfilemodule",  # Name of the module
+    "ragfile.ragfile",
     sources=[
         "ragfile/ragfilemodule.c",  # Python binding source file
         "src/core/ragfile.c",
